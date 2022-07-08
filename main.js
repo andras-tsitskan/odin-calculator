8de093820b4540cd6c288352b9cf5678246cb2bb
Andraš Tšitškan <andras.tsitskan@gmail.com>
2022-07-08 21:15:28 +0300

    Update main maths functions
    Make sure maths functions operate on numbers, not strings, by converting input to numbers.

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 "use strict";
 
 function add(a, b) {
-  return a + b;
+  return +a + +b;
 }
 
 function subtract(a, b) {
-  return a - b;
+  return +a - +b;
 }
 
 function multiply(a, b) {
-  return a * b;
+  return +a * +b;
 }
 
 function divide(a, b) {
-  return b === 0 ? "error" : a / b;
+  return +b === 0 ? "error" : +a / +b;
 }
 
 function operate(operator, num1, num2) {

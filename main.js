9169ad67c0d9237537e28d1cde76ba8e42c82861
Andraš Tšitškan <36357686+andras-tsitskan@users.noreply.github.com>
2022-07-08 10:26:42 +0300

    Add operate()
    Takes an operator and 2 numbers and then calls the corresponding maths function.

--- main.js
+++ main.js
@@ -15,3 +15,18 @@ function multiply(a, b) {
 function divide(a, b) {
   return b === 0 ? "error" : a / b;
 }
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "add":
+      return add(num1, num2);
+    case "subtract":
+      return subtract(num1, num2);
+    case "multiply":
+      return multiply(num1, num2);
+    case "divide":
+      return divide(num1, num2);
+    default:
+      break;
+  }
+}

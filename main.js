921a1f5ff15ec3b09ceb00efbdba1c0e9a5ba02c
Andraš Tšitškan <36357686+andras-tsitskan@users.noreply.github.com>
2022-07-08 10:15:41 +0300

    Add main maths functions
    add()
    subtract()
    multiply()
    divide()

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 "use strict";
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return b === 0 ? "error" : a / b;
+}
